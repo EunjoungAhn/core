92383d20890a9e622fe2b653c26a95a42ce8157a
EunjoungAhn <aej9300@gmail.com>
2022-08-30 19:20:15 +0900

    스프링 빈 설정 메타 정보 - BeanDefinition

--- src/test/java/hello/core/xml/XmlAppContext.java
+++ src/test/java/hello/core/xml/XmlAppContext.java
@@ -8,11 +8,13 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 import static org.assertj.core.api.Assertions.*;
 
 public class XmlAppContext {
-
     @Test
-    void xmlAppContext(){
-        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
-        MemberService memberService = ac.getBean("memberService", MemberService.class);
+    void xmlAppContext() {
+        ApplicationContext ac = new
+                GenericXmlApplicationContext("appConfig.xml");
+
+        MemberService memberService = ac.getBean("memberService",
+                MemberService.class);
         assertThat(memberService).isInstanceOf(MemberService.class);
     }
 }
