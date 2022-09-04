24886e4db0fac2354fd39caee74d1bb807cbd7a1
EunjoungAhn <aej9300@gmail.com>
2022-09-05 00:43:13 +0900

    애노테이션 @PostConstruct, @PreDestroy <-! 위의 2가지 말고, 방법을 쓰면된다! !! 대신 단점으로 외부 라이브러리에 적용을 못한다. 그러므로 외부 라이브러리를 사용하려면 위의 방법을 쓰면 된다.

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -15,7 +15,8 @@ public class BeanLifeCycleTest {
     }
     @Configuration
     static class LifeCycleConfig {
-        @Bean(initMethod = "init", destroyMethod = "close")
+        //@Bean(initMethod = "init", destroyMethod = "close") 외부 라이브러리를 사용하려면 이 방법을 쓰면 된다.
+        @Bean
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev");
