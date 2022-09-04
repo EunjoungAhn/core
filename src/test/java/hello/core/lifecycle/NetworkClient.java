a7b7bea0d686a6e23724fc64cb589ff582193256
EunjoungAhn <aej9300@gmail.com>
2022-09-05 00:33:31 +0900

    인터페이스 InitializingBean, DisposableBean <- 현재 이 초기화, 소멸 인터페이스는 나온지 오래되어서 사용하지 않는다.

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,10 +1,12 @@
 package hello.core.lifecycle;
-public class NetworkClient {
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
     private String url;
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
     public void setUrl(String url) {
         this.url = url;
@@ -19,5 +21,18 @@ public class NetworkClient {
     //서비스 종료시 호출
     public void disconnect() {
         System.out.println("close: " + url);
+    }
+
+    @Override //의존 관계 주입이 끝나면 부르는 것
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.disconnect");
+        disconnect();
     }
 }
