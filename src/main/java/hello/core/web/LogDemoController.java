ed13206a986d556a7470174d1d7153e6ea29ab19
EunjoungAhn <aej9300@gmail.com>
2022-09-05 21:40:25 +0900

    스코프와 프록시

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -1,24 +1,22 @@
 package hello.core.web;
-
 import hello.core.common.MyLogger;
+import hello.core.web.LogDemoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
-
 @Controller
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProviderObject;
-
+    private final MyLogger myLogger;
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
-        String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProviderObject.getObject();
+        System.out.println(" myLogger = " + myLogger.getClass());
+
+        String requestURL = request.getRequestURL().toString(); //CGLIB라는 라이브러리로 내 클래스를 상속 받은 가짜 프록시 객체를 만들어서 주입한다.
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
